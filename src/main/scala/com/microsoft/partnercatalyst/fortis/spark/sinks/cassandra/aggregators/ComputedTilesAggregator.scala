258f64f041d46ce10d04a02afc6b42b9f6d48305
erisch <erik.schlegel@gmail.com>
2017-08-26 14:29:46 -0500

    Fixing issues with how externalsources are being aggregated

--- src/main/scala/com/microsoft/partnercatalyst/fortis/spark/sinks/cassandra/aggregators/ComputedTilesAggregator.scala
+++ src/main/scala/com/microsoft/partnercatalyst/fortis/spark/sinks/cassandra/aggregators/ComputedTilesAggregator.scala
@@ -21,7 +21,7 @@ class ComputedTilesAggregator extends FortisAggregatorBase with Serializable {
   }
 
   private def DetailedTileAggregateViewQuery(includeExternalSource: Boolean, includePipelinekey: Boolean): String = {
-    val SelectClause = (SelectableColumnNames ++ Seq(ParseColumnSelect(PipelineKeyColumnName, includeExternalSource), ParseColumnSelect(ExternalSourceColumnName, includeExternalSource), DetailTileIdColumnName)).mkString(",")
+    val SelectClause = (SelectableColumnNames ++ Seq(ParseColumnSelect(PipelineKeyColumnName, includePipelinekey), ParseColumnSelect(ExternalSourceColumnName, includeExternalSource), DetailTileIdColumnName)).mkString(",")
     val GroupedColumns =  (GroupedBaseColumnNames ++ Seq(DetailTileIdColumnName)).mkString(",")
 
     s"SELECT $SelectClause, $AggregateFunctions " +
@@ -84,7 +84,7 @@ class ComputedTilesAggregator extends FortisAggregatorBase with Serializable {
 
   override def AggregateEventBatches(session: SparkSession, flattenedEvents: DataFrame): DataFrame = {
     val detailedAggDF = AggregateComputedTiles(session=session, sourceTablename="detailedTileView", includeExternalSource=true, includePipelinekey=true)
-    val pipelineKeyOnlyAggDF = AggregateComputedTiles(session=session, sourceTablename="sourcesOnlyTileView", includeExternalSource=false, includePipelinekey=true)
+    val pipelineKeyOnlyAggDF = AggregateComputedTiles(session=session, sourceTablename="sourcesOnlyTileView", includeExternalSource=true, includePipelinekey=false)
     val tilesAndPeriodOnlyAggDF = AggregateComputedTiles(session=session, sourceTablename="onlyTilesView", includeExternalSource=false, includePipelinekey=false)
     val unionedResults = detailedAggDF.union(pipelineKeyOnlyAggDF).union(tilesAndPeriodOnlyAggDF)
 
