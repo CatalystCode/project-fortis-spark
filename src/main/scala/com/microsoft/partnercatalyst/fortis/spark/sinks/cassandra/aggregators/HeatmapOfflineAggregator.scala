d6a020abf20ff7ff5b9ebf1425d331b9afedf2a3
erisch <erik.schlegel@gmail.com>
2017-10-01 15:56:25 -0500

    rebasing

--- src/main/scala/com/microsoft/partnercatalyst/fortis/spark/sinks/cassandra/aggregators/HeatmapOfflineAggregator.scala
+++ src/main/scala/com/microsoft/partnercatalyst/fortis/spark/sinks/cassandra/aggregators/HeatmapOfflineAggregator.scala
@@ -2,14 +2,12 @@ package com.microsoft.partnercatalyst.fortis.spark.sinks.cassandra.aggregators
 
 import com.datastax.spark.connector.writer.SqlRowWriter
 import com.microsoft.partnercatalyst.fortis.spark.dba.ConfigurationManager
-import com.microsoft.partnercatalyst.fortis.spark.sinks.cassandra.dto.{Event, HeatmapTile}
+import com.microsoft.partnercatalyst.fortis.spark.sinks.cassandra.dto.{ComputedTile, Event, HeatmapTile}
 import com.microsoft.partnercatalyst.fortis.spark.sinks.cassandra.{CassandraHeatmapTiles, CassandraTileBucket}
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.SparkSession
-
 import com.datastax.spark.connector._
 
-class HeatmapOfflineAggregator(session: SparkSession, configurationManager: ConfigurationManager) extends OfflineAggregator[HeatmapTile] {
+class HeatmapOfflineAggregator(configurationManager: ConfigurationManager) extends OfflineAggregator[HeatmapTile] {
   override def aggregate(events: RDD[Event]): RDD[HeatmapTile] = {
     val siteSettings = configurationManager.fetchSiteSettings(events.sparkContext)
     val tiles = events.flatMap(CassandraHeatmapTiles(_, siteSettings.defaultzoom))
@@ -29,8 +27,8 @@ class HeatmapOfflineAggregator(session: SparkSession, configurationManager: Conf
     }).values
   }
 
-  private def aggregateAndSaveTileBuckets(tiles: RDD[HeatmapTile], keyspace: String): Unit = {
-    val tilesComputed = tiles.keyBy(r => {
+  def aggregateTileBuckets(tiles: RDD[HeatmapTile]): RDD[ComputedTile] = {
+    tiles.keyBy(r => {
       (
         r.periodtype, r.perioddate,
         r.conjunctiontopic1, r.conjunctiontopic2, r.conjunctiontopic3,
@@ -45,7 +43,10 @@ class HeatmapOfflineAggregator(session: SparkSession, configurationManager: Conf
         avgsentimentnumerator = sentimentNumerator
       )
     }).values.map(CassandraTileBucket(_))
+  }
 
+  private def aggregateAndSaveTileBuckets(tiles: RDD[HeatmapTile], keyspace: String): Unit = {
+    val tilesComputed = aggregateTileBuckets(tiles)
     val reparted = tilesComputed.repartitionByCassandraReplica("fortis", "computedtiles")
 
     val updatedRows = reparted.leftJoinWithCassandraTable("fortis", "computedtiles").map(pair => {
